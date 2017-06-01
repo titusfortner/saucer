cf6e70dd078ec3555e5eba2eed4e1bf1b8c716ae
Titus Fortner <titusfortner@gmail.com>
2017-05-31 17:12:31 -0700

    support no configs defined and configs defined in yaml

--- lib/saucer/parallel.rb
+++ lib/saucer/parallel.rb
@@ -1,4 +1,5 @@
 require 'sauce_platforms'
+require 'yaml'
 
 module Saucer
   class Parallel
@@ -7,30 +8,30 @@ module Saucer
 
     def initialize(opt = {})
       @number = opt[:number] || ENV['PARALLEL_PROCESSES']
-      @path = opt[:path] || ENV['TEST_PATH'] || "/spec"
+      @path = opt[:path] || ENV['TEST_PATH'] || "spec"
       @output = opt[:output] || ENV['REPORT_OUTPUT'] || 'output'
       @platforms = opt[:platforms] || []
+      if File.exist?('configs/platform_configs.yml')
+        yaml = YAML.load_file('configs/platform_configs.yml')
+        @platforms ||= yaml.map { |c| Saucer::PlatformConfiguration.new(c) }
+      end
       @success = true
     end
 
     def run
+      if @platforms.empty?
+        raise StandardError, "Tests failed!" unless execute_test
+        return
+      end
+
       @platforms.each do |platform|
         Rake::Task.define_task(platform.name) {
           ENV['platform'] = platform.to_hash[:platform]
           ENV['browser_name'] = platform.to_hash[:browserName]
           ENV['version'] = platform.to_hash[:version]
-          ENV['build'] = 'test4'
 
           begin
-            command = if path.include?('spec')
-                        ENV['PARALLEL_SPLIT_TEST_PROCESSES'] = number.to_s if number
-                        "parallel_split_test #{path} --out #{@output}.xml"
-                      elsif path.include?('features')
-                        "parallel_cucumber #{path} -o \"--format junit --out #{@output} --format pretty\" -n #{number.to_s}"
-                      else
-                        raise ArgumentError, "Unable to determine if using rspec or cucumber"
-                      end
-            @result = system command
+            @result = execute_test
           ensure
             @success &= @result
           end
@@ -42,5 +43,15 @@ module Saucer
       }.invoke
     end
 
+    def execute_test
+      if path.include?('spec')
+        ENV['PARALLEL_SPLIT_TEST_PROCESSES'] = number.to_s if number
+        system "parallel_split_test #{path} --out #{@output}.xml"
+      elsif path.include?('features')
+        system "parallel_cucumber #{path} -o \"--format junit --out #{@output} --format pretty\" -n #{number.to_s}"
+      else
+        raise ArgumentError, "Unable to determine if using rspec or cucumber"
+      end
+    end
   end
 end
