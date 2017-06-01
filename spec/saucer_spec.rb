d56eac26b1a9ccd40b8de0deea95c8313997ecf6
Titus Fortner <titusfortner@gmail.com>
2017-05-31 17:33:42 -0700

    Don't need the extra namespace included

--- spec/saucer_spec.rb
+++ spec/saucer_spec.rb
@@ -4,30 +4,30 @@ RSpec.describe Saucer do
 
   after { @driver.quit if @driver }
   it 'initializes browser' do
-    @driver = Saucer::Driver.new
+    @driver = Driver.new
     expect(@driver).to be_a Selenium::WebDriver::Driver
   end
 
   it 'uses config parameters' do
-    params = Saucer::Config::Selenium.new.config_params
+    params = Config::Selenium.new.config_params
     expect(params.size).to eq 32
   end
 
   it 'sets capabilities' do
-    config_selenium = Saucer::Config::Selenium.new(version: 'foo', browser_name: :chrome, command_timeout: 4)
+    config_selenium = Config::Selenium.new(version: 'foo', browser_name: :chrome, command_timeout: 4)
     caps = config_selenium.capabilities
     expect(caps[:version]).to eq 'foo'
     expect(caps[:command_timeout]).to eq 4
   end
 
   it 'uses capabilities to initialize browser' do
-    config_selenium = Saucer::Config::Selenium.new(version: '53', browser_name: :firefox)
-    @driver = Saucer::Driver.new(config_selenium)
+    config_selenium = Config::Selenium.new(version: '53', browser_name: :firefox)
+    @driver = Driver.new(config_selenium)
     expect(@driver.capabilities['browserVersion']).to eq '53.0'
   end
 
   it 'uses annotations' do
-    @driver = Saucer::Driver.new
+    @driver = Driver.new
     @driver.get('http://google.com')
     @driver.comment('Hi Mom!')
     @driver.job_result(true)
@@ -37,7 +37,7 @@ RSpec.describe Saucer do
   end
 
   it 'uses Sauce Whisk' do
-    @driver = Saucer::Driver.new
+    @driver = Driver.new
     expect(@driver.sauce.account.username).to eq ENV['SAUCE_USERNAME']
     expect(@driver.sauce.job.id).to eq(@driver.session_id)
     expect(@driver.sauce.concurrency[:total_concurrency]).to eq 100
